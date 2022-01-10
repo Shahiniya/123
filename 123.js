8bf55e6c9a1f9ae7b6d153590dee7f1a0c4448c6
Shelli <sh.panjieva@gmail.com>
2022-01-10 17:11:49 +0900

    new

---
+++ 123.js
@@ -0,0 +1,16 @@
+var arr = [1,2,3,2,4,5,3,7]
+const getCompany =()=>{
+   var a = [];
+
+      //  arr.map((value)=>{
+      //    if(value > 2 && value <=5){
+      //       a.push(value)
+      // }
+      //  })
+      // console.log(a);
+
+      arr.fill(10,1,4);
+      console.log(arr);
+
+}
+ getCompany();
